cc931a283acd3b88aabac19556928b1e5e4fec3a
shenwl <shenweilun96@gmail.com>
2018-03-23 15:18:55 +0800

    cross domain

---
+++ cross_domain.py
@@ -0,0 +1,8 @@
+def allow_cross_domain(fun):
+    @wraps(fun)
+    def wrapper_fun(*args, **kwargs):
+        rst = make_response(fun(*args, **kwargs))
+        rst.headers['Access-Control-Allow-Origin'] = '*'
+        rst.headers['Access-Control-Allow-Methods'] = 'POST'
+        return rst
+    return wrapper_fun
